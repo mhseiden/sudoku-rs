31bbf3b6fa6f80f90a7661cb12af2f4a13c63022
Max Seiden <max@sigmacomputing.io>
2017-09-25 09:45:53 -0700

    use puzzle from the wiki (and verify the result is correct)

--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,35 @@ extern crate sudoku;
 fn main() {
     env_logger::init().unwrap();
 
+    // This is the 17-hint sudoku, which is specially designed to foil
+    // backtracking algorithms. This method only needs 227 guesses.
+    //
+    // https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
     let solution = sudoku::solve(vec![
-        (0, 2, 7),
-        (0, 4, 8),
-        (0, 6, 1),
-
-        (1, 1, 9),
-        (1, 3, 1),
         (1, 5, 3),
         (1, 7, 8),
+        (1, 8, 5),
 
-        (2, 0, 5),
-        (2, 4, 4),
-        (2, 8, 6),
+        (2, 2, 1),
+        (2, 4, 2),
 
-        (3, 1, 1),
-        (3, 7, 4),
+        (3, 3, 5),
+        (3, 5, 7),
 
-        (4, 0, 8),
-        (4, 2, 2),
-        (4, 6, 6),
-        (4, 8, 7),
+        (4, 2, 4),
+        (4, 6, 1),
 
-        (5, 1, 6),
-        (5, 7, 5),
+        (5, 1, 9),
 
-        (6, 0, 4),
-        (6, 4, 6),
-        (6, 8, 2),
+        (6, 0, 5),
+        (6, 7, 7),
+        (6, 8, 3),
 
-        (7, 1, 2),
-        (7, 3, 8),
-        (7, 5, 4),
-        (7, 7, 1),
+        (7, 2, 2),
+        (7, 4, 1),
 
-        (8, 2, 9),
-        (8, 4, 5),
-        (8, 6, 4),
+        (8, 4, 4),
+        (8, 8, 9),
     ]);
     println!("{}", solution);
 }
